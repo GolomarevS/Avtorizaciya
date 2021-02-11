8cc0b4a6348b6fa30eee4862c022a547a6886bf6
Golomarev Sergei <maloeidd@gmail.com>
2021-02-11 10:10:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/avtorizaciya/second.java
@@ -0,0 +1,13 @@
+package com.example.avtorizaciya;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class second extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondmain);
+    }
+}
